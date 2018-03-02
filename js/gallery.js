c332675260ba5414d24dfa4f37c287ebde8f2439
PJ623 <jardiolin.paolo@gmail.com>
2018-03-01 23:21:12 -0800

    modularized code, implemented modal view

--- js/gallery.js
+++ js/gallery.js
@@ -1,28 +1,76 @@
-function getGallery() {
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", "/json/gallery.json");
-    xhr.onload = () => {
-        if (xhr.status == 200) {
+function Gallery() {
 
-            let galleryContainer = document.getElementById("galleryContainer");
-            let gallery = JSON.parse(xhr.response);
+    // Really just contains objects containing image file names at the moment.
+    this.images = [];
 
-            for (let i = 0; i < gallery.images.length; i++) {
-                let image = document.createElement("IMG");
-                image.className = "thumbnail";
-                image.src = "/images/" + gallery.images[i].fileName;
+    // Parses JSON and adds the image data from that JSON to the Gallery object.
+    this.insertImages = (galleryJSON) => {
+        if (galleryJSON) {
+            let receivedGallery = JSON.parse(galleryJSON);
 
-                // Redo later. Turn into modal view
-                image.onclick = () => {
-                    if (image.style.width != "auto") {
-                        image.style.width = "auto";
-                    } else {
-                        image.style.width = "200px";
-                    }
-                }
-                galleryContainer.appendChild(image);
+            for (let i = 0; i < receivedGallery.images.length; i++) {
+                this.images.push(receivedGallery.images[i]);
             }
         }
+    }
+
+    // Renders onto given HTML element, 'ele'.
+    // TODO: Modularize this method. Maybe take a call back to hook up the onclick stuff.
+    this.render = (ele) => {
+        for (let i = 0; i < this.images.length; i++) {
+            let image = document.createElement("IMG");
+
+            image.className = "thumbnail";
+            image.src = "/images/" + this.images[i].fileName;
+
+            if (document.getElementById("modal") && document.getElementById("modal-content")) {
+                var modal = document.getElementById("modal");
+                var modalContent = document.getElementById("modal-content");
+            } else {
+                var modal = document.createElement("DIV");
+                modal.id = "modal";
+                modal.style.display = "none";
+
+                var modalContent = document.createElement("IMG");
+                modalContent.id = "modal-content";
+
+                document.body.appendChild(modal);
+                modal.appendChild(modalContent);
+            }
+
+            image.onclick = () => {
+                if (modal.style.display == "none") {
+                    modal.style.display = "flex";
+                    modalContent.src = image.src;
+                }
+            }
+
+            modal.onclick = () => {
+                modal.style.display = "none";
+                modalContent.src = "";
+            }
+
+            ele.appendChild(image);
+        }
+    }
+}
+
+// Create gallery object.
+let gallery = new Gallery();
+
+// Uses AJAX to request a JSON file containing gallery data.
+function getGallery() {
+    let xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "/json/gallery.json");
+
+    xhr.onload = () => {
+        if (xhr.status == 200) {
+            let galleryContainer = document.getElementById("galleryContainer");
+            gallery.insertImages(xhr.response);
+            gallery.render(galleryContainer);
+        }
     };
+
     xhr.send();
 }
